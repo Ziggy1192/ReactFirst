e1e02b7309bb1cb2d79ae42156cc972ebc7c4aee
Dziugas Bartkevicius <bartkevicius.dziugas@gmail.com>
2022-07-25 21:39:47 +0100

    filter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
@@ -8,9 +8,11 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
+    // console.log('cosntructor');
   }
 
   componentDidMount() {
+    // console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => response.json())
     .then((users) => this.setState(() => {
@@ -23,11 +25,30 @@ class App extends Component {
   }
 
   render(){
+    // console.log('render');
     return (
       <div className="App">
-        {
-          this.state.monsters.map((monster) => {
-            return <div key={monster.id}><h1>{monster.name} {monster.email}</h1></div>
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search monsters'
+          onChange={(event) => {
+            console.log(event.target.value);
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.includes(event.target.value);
+            });
+
+            this.setState=(() => {
+              return { monsters: filteredMonsters };
+            });
+          }}
+          />
+          {this.state.monsters.map((monster) => {
+            return (
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>
+            );
           })}
       </div>
     );
